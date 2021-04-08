671f672fd14b9811452ad25d50a28b562ec12c81
sakibchowdhury131 <sakibchowdhury131@gmail.com>
2021-04-08 15:56:55 +0600

    UART communication and MQTT server included

--- firmware/powerMeasure/powerMeasure.ino
+++ firmware/powerMeasure/powerMeasure.ino
@@ -1,5 +1,9 @@
 /* Author: Sakib Chowdhury */
 
+
+#include <ArduinoJson.h>
+#include <SoftwareSerial.h>
+
 /* ACS712 Model         Optimized Current Range              Output Sensitivity
 -------------------------------------------------------------------------------
 
@@ -25,10 +29,12 @@ int voltages[samplerate], currents[samplerate];
 float tempV, tempI;
 int voltage_offset = 512, current_offset = 512;
 double Voltage = 0, Amps = 0, Power=0, PF;
+SoftwareSerial linkSerial(10, 11); // RX, TX
 
 void setup()
 {
   Serial.begin(115200);
+  linkSerial.begin(4800);
 
 
 }
@@ -90,7 +96,7 @@ void loop()
     //Serial.println(voltages[i]);
     tempV = 5*(11.0*220.0/6)*(voltages[i])/1023.0*Vcorr; // 220 - 6V transformer and 10k and 1k resistor in series in the 6V outlet
     tempI = (5000.0*(currents[i])) / (1023.0*mVperAmp)*ccorr ; // Gets you mV
-    Serial.print(tempV); Serial.print("    "); Serial.println(tempI);
+    //Serial.print(tempV); Serial.print("    "); Serial.println(tempI);
     Power += tempV*tempI;
     
     Voltage = Voltage + pow(tempV,2);
@@ -116,11 +122,16 @@ void loop()
   float pf = (Power/(Voltage*Amps));
   Serial.println (pf); 
   //display.clearDisplay();
-  
 
-
-   str = "Real power : " + String (Power) + "Watt" + "  " + "Current :" + String (Amps) + "A" + "  " + "Supply Voltage:" + String (Voltage) + "Volts" + "  " + "Power Factor :" + String (pf) ;
-   Serial.print (str);
-   delay (2000);
+  StaticJsonDocument<400> doc;
+  doc["Voltage"] = Voltage;
+  doc["Current"] = Amps;
+  doc["Real Power"] = Power;
+  doc["PF"] = pf;
+  serializeJson(doc, linkSerial);
+ 
+  str = "Real power : " + String (Power) + "Watt" + "  " + "Current :" + String (Amps) + "A" + "  " + "Supply Voltage:" + String (Voltage) + "Volts" + "  " + "Power Factor :" + String (pf) ;
+  Serial.print (str);
+  delay (10000);
    
 }
